9557f75d76dbc7afdce470c11e2c40ae65ea32df
ashiq <ishtiaqashiq5@gmail.com>
2022-09-08 00:11:12 -0400

    No module named api bug fixed

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import Edit, Initialize, Sign
+from .views import Edit, Init, Sign
 
 app_name = 'api'
 
 urlpatterns = [
     path('edit/', Edit.as_view(), name='edit'),
     path('sign/', Sign.as_view(), name='sign'),
-    path('init/', Initialize.as_view(), name='init'),
+    path('init/', Init.as_view(), name='init'),
 ]
