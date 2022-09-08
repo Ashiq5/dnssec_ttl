1767cd9ecf2e3b6b19c130994d145f186ac9c95b
ashiq <ishtiaqashiq5@gmail.com>
2022-09-08 01:09:41 -0400

    minor

--- api/views.py
+++ api/views.py
@@ -38,7 +38,7 @@ def _edit_zone_file(container_id, ttl, exp_id):
         zone_file_name = "db." + domain
         path = os.path.join(path, zone_file_name)
         with open(path, 'a') as f:
-            f.write('*.' + exp_id + '	IN	A	' + container2ip_dict[container_id] + '\n')
+            f.write('*.' + exp_id + '	IN	A	' + container2ip_dict[str(container_id)] + '\n')
 
         cmd = "docker exec -i " + containers[container_id-1] + " sh -c 'cat > /etc/bind/zones/" + zone_file_name \
               + "' < " + path
