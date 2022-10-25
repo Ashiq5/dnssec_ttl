44500af55197ebe856b06921c8668feed78d60db
ashiq <ishtiaqashiq5@gmail.com>
2022-10-25 14:47:06 -0400

    added logging

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import Edit, Init, Sign, Edit_Sign, BindUpdateViewV2
+from .views import Init, Edit_Sign, BindUpdateViewV2
 
 app_name = 'api'
 
 urlpatterns = [
-    path('edit/', Edit.as_view(), name='edit'),
-    path('sign/', Sign.as_view(), name='sign'),
+    # path('edit/', Edit.as_view(), name='edit'),
+    # path('sign/', Sign.as_view(), name='sign'),
     path('init/', Init.as_view(), name='init'),
     path('edit-sign/', Edit_Sign.as_view(), name='edit-sign'),
     path('update-bind/v2/', BindUpdateViewV2.as_view(), name='bind_update_api_v2'),
